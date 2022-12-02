107535032ac074d1675b36806b1e7e02080e24af
Stober <CHRISTIAN_STOBER@dpsk12.org>
2022-12-02 08:48:15 -0700

    Updated templated

--- template.py
+++ template.py
@@ -10,7 +10,10 @@ def parse_line(line):
     #return [int(i) for i in re.findall('-?\d+', line)] # grab all the numbers
 
 #chunks = [chunk.splitlines() for chunk in open(FNAME).read().split('\n\n')]
+#data = [[parse_line(line) for line in chunk] for chunk in chunks]
+
 data = [parse_line(line) for line in open(FNAME).read().splitlines()] # in chunks[0]]
+
 #data = [int(i) for i in open(FNAME).read().split(',')]
 
 
