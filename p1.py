3072c356af574424efb129248aab9f83c2bed8ea
Stober <CHRISTIAN_STOBER@dpsk12.org>
2022-12-02 08:48:00 -0700

    Deleted unused code

--- p1.py
+++ p1.py
@@ -1,9 +1,6 @@
 #import pdb
 
 FNAME = "in1.txt"
-    
-def parse_line(line):
-    return int(line)
 
 chunks = [chunk.splitlines() for chunk in open(FNAME).read().split('\n\n')]
 data = [sum([int(line) for line in chunk]) for chunk in chunks]
