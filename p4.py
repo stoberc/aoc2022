fa5cc9b5b2e93bcc8379552849ffbf6c95290ee6
Stober <CHRISTIAN_STOBER@dpsk12.org>
2022-12-04 22:22:45 -0700

    Simplified overlap logic

--- p4.py
+++ p4.py
@@ -1,4 +1,3 @@
-#import pdb
 import re
 
 FNAME = "in4.txt"
@@ -6,9 +5,12 @@ FNAME = "in4.txt"
 def parse_line(line):
     return [int(i) for i in re.findall('\d+', line)] # grab all the numbers
 
+# check if the interval [a, b] is a subset of the interval [c, d]
+# or vice versa
 def contains(a, b, c, d):
     return a <= c and b >= d or c <= a and d >= b
     
+# check if the interval [a, b] intersects the interval [c, d]
 def overlaps(a, b, c, d):
     return b >= c and a <= d
 
@@ -16,6 +18,3 @@ data = [parse_line(line) for line in open(FNAME).read().splitlines()]
 
 print("Part 1:", sum([contains(*i) for i in data]))
 print("Part 2:", sum([overlaps(*i) for i in data]))
-
-#pdb.set_trace()
-    
